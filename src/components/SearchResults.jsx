759361f1037501a1f2b84592130f699a2d6315e4
Zoe McGee <zoe.c.mcgee@gmail.com>
2026-08-06 10:16:25 -0500

    fixing wierd bug from vercel

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,16 +1,4 @@
 
-// import React from 'react';
-// import { Result } from '../components/Result';
-
-// export const SearchResults = ({ results }) => {
-//   return (
-//     <div className="absolute top-0 left-0 right-0 bg-white border-2 border-gray-200 rounded-xl shadow-xl max-h-60 overflow-y-auto mt-1 divide-y divide-gray-100">
-//       {results.map((result) => (
-//         <Result key={result.id || result.name} result={result} />
-//       ))}
-//     </div>
-//   );
-// };
 
 import React from 'react';
 import { Result } from './Result';
